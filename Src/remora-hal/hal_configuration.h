47a4bd962f12994746a10e9ae25b59e785da60d3
Ben <ben_jacobson@live.com>
2025-08-05 20:53:02 +1000

    updates to linker script and platformio file to prevent overwriting allocated config flash memory

--- Src/remora-hal/hal_configuration.h
+++ Src/remora-hal/hal_configuration.h
@@ -8,7 +8,8 @@
 // these support F401xx, F411xx and F446xx
 namespace HAL_Config {
     constexpr std::uintptr_t JSON_storage_address           = 0x8060000;
-    constexpr std::uintptr_t JSON_upload_address            = 0x8040000;   
+    constexpr std::uintptr_t JSON_upload_address            = 0x8060000; // store in same address to save on FLASH
+
     constexpr std::uintptr_t user_flash_last_page_address   = JSON_storage_address;
     constexpr std::uintptr_t user_flash_end_address         = JSON_storage_address + (128 * 1024) - 1;
     constexpr uint32_t JSON_Sector                          = FLASH_SECTOR_7;
