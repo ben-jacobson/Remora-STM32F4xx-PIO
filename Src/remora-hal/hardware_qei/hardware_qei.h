5c25d60391338fc5b187894b531a3d153e7a385d
Ben <ben_jacobson@live.com>
2025-12-22 13:45:40 +1100

    Testing new Remore-core softEncoder module, functioning as expected. Ported in the hardware QEI code but quite a bit of work left to do there.

---
+++ Src/remora-hal/hardware_qei/hardware_qei.h
@@ -0,0 +1,31 @@
+#ifndef QEIDRIVER_H
+#define QEIDRIVER_H
+
+#include "stm32f4xx_hal.h"
+
+// class hardware_qei
+// {
+//     private:
+
+//         TIM_HandleTypeDef       htim;
+//         TIM_Encoder_InitTypeDef sConfig  = {0};
+//         TIM_MasterConfigTypeDef sMasterConfig  = {0};
+
+//         //InterruptIn             qeiIndex;
+//         IRQn_Type 		        irq;
+
+//         bool                    hasIndex;
+//         bool                    indexDetected;
+//         int32_t                 indexCount;
+
+//         void interruptHandler();
+
+//     public:
+//         hardware_qei();            // for channel A & B
+//         hardware_qei(bool hasIndex);        // For channels A & B, and index
+
+//         void init(void);
+//         uint32_t get(void);
+// };
+
+#endif
